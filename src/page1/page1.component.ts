d32108c929e837d9be91f7ed3792a5d72911b570
fransiscus888 <c14180113@john.petra.ac.id>
2021-03-02 19:54:11 +0700

    2 files modified

--- src/page1/page1.component.ts
+++ src/page1/page1.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { GlobalvarService } from '../app/globalvar.service';
+import { Component, OnInit } from "@angular/core";
+import { GlobalvarService } from "../app/globalvar.service";
 
 @Component({
-  selector: 'app-page1',
-  templateUrl: './page1.component.html',
-  styleUrls: ['./page1.component.css']
+  selector: "app-page1",
+  templateUrl: "./page1.component.html",
+  styleUrls: ["./page1.component.css"]
 })
 export class Page1Component implements OnInit {
-  judul : String
-  isi : String
-  tanggal: String
+  judul: String;
+  isi: String;
+  tanggal: String;
   myDate = new Date();
-  constructor(public globvar : GlobalvarService) { }
+  constructor(public globvar: GlobalvarService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  add(){
-    this.tanggal=this.myDate.toString()
-    this.globvar.addnote(this.judul,this.isi,this.tanggal);
-    this.judul=""
-    this.isi=""
-    alert("Berhasil add Note")
+  add() {
+      this.tanggal = this.myDate.toString();
+      this.globvar.addnote(this.judul, this.isi, this.tanggal);
+      this.judul = "";
+      this.isi = "";
+      alert("Berhasil add Note");
   }
 }
